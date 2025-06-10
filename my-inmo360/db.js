237bcfbe15bbf6e029f61da70a66a00c9aab46d6
Jose20Longo02 <JoseLongo@Medialy.Agency>
2025-06-10 08:08:07 -0600

    Update db.js

--- my-inmo360/db.js
+++ my-inmo360/db.js
@@ -1,14 +1,10 @@
-// db.js — Configuración para usar DATABASE_URL en producción
+// db.js
 const { Pool } = require('pg');
 
-// Leer la cadena de conexión de la variable de entorno
-const connectionString = process.env.DATABASE_URL;
-
-// Configurar Pool para Postgres
 const pool = new Pool({
-  connectionString,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false  // necesario para clusters gestionados que requieren SSL
+    rejectUnauthorized: false
   }
 });
 
